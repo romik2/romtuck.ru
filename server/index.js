b254b6171bcf25d6f05aa9819a20fde2053540e2
tarkinrs <roman.tarkin@npo-at.com>
2022-12-13 18:22:41 +0500

    test

--- server/index.js
+++ server/index.js
@@ -4,46 +4,22 @@ const https = require('https');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./database.sql.local');
 const tablesName = ['work', 'education', 'experience', 'certificate'];
-const config = require('./config.json');
 
-if (config.protocol == 'wss') {
-    const server = https.createServer({
-        cert: fs.readFileSync(`${config.cert_dirname}`),
-        key: fs.readFileSync(`${config.key_dirname}`),
-        ca: fs.readFileSync(`${config.ca_dirname}`)
-    });
-    const wsServer = new WebSocket.Server({ server, port: config.port});
+const wsServer = new WebSocket.Server({port: 4567});
 
-    wsServer.on('connection', function (client, req) {
-        console.log(`Новый пользователь: ${req.socket.remoteAddress}`);
-    
-        sendMessages(client);
-        
-        setInterval(function () {
-            sendMessages(client)
-        }, 6000);
-    
-        client.on('close', function() {
-            console.log(`Пользователь отключился: ${req.socket.remoteAddress}`);
-        })
-    });
-} else {
-    const wsServer = new WebSocket.Server({port: config.port});
+wsServer.on('connection', function (client, req) {
+    console.log(`Новый пользователь: ${req.socket.remoteAddress}`);
 
-    wsServer.on('connection', function (client, req) {
-        console.log(`Новый пользователь: ${req.socket.remoteAddress}`);
+    sendMessages(client);
     
-        sendMessages(client);
-        
-        setInterval(function () {
-            sendMessages(client)
-        }, 6000);
-    
-        client.on('close', function() {
-            console.log(`Пользователь отключился: ${req.socket.remoteAddress}`);
-        })
-    });
-}
+    setInterval(function () {
+        sendMessages(client)
+    }, 6000);
+
+    client.on('close', function() {
+        console.log(`Пользователь отключился: ${req.socket.remoteAddress}`);
+    })
+});
 
 function sendMessages(client) {
     tablesName.forEach(name => {
--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import data from './config.json';
 
-const url = 'ws://192.168.1.157:4567';
-var server = new WebSocket(url);
-
-server.onerror = () => {
-  window.location.reload();
-};
+var server = new WebSocket(data.ws);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
